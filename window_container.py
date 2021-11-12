8b5341a4f29c1cc2e559496ae264a6b65df744cd
Mark Fisher <markdfisher38@gmail.com>
2021-11-11 22:07:01 -0500

    Add files via upload

--- window_container.py
+++ window_container.py
@@ -28,81 +28,9 @@ import win32gui
 import win32con
 
 from window_capture import capture_window
+from window_monitor import WindowMonitor
 
 user32 = ctypes.windll.user32
-
-class WindowOverlapMonitor:
-    def __init__(self):
-        self._timer = QTimer()
-        self._timer.timeout.connect(self._timeout)
-        self._timer.start(200)
-        self._rectangles = []
-        self._z_positions = {}
-        self._widgets = []
-        self._previous_overlaps = []
-        self._previous_rectangles = {}
-
-    def register(self, widget):
-        self._widgets.append(widget)
-        self._previous_overlaps.append(set())
-
-    def _check_for_overlap_changes(self):
-        previous_overlaps = self._previous_overlaps
-        previous_rectangles = self._previous_rectangles
-
-        for widget_index, widget in enumerate(self._widgets):
-            position = widget.mapToGlobal(QPoint(0,0))
-            rectangle = QRect(position, widget.size())
-
-            # Test windows above the window containing the widget
-            current_overlaps = set()
-            window_handle = widget.window().effectiveWinId()
-            window_z = self._z_positions[window_handle]
-            for test_handle, test_rectangle in self._rectangles[:window_z]:
-                if not rectangle.intersects(test_rectangle):
-                    continue
-
-                current_overlaps.add(test_handle)
-
-                if test_handle not in previous_overlaps[widget_index]:
-                    widget.overlap_enter_event(test_handle, test_rectangle)
-                    previous_rectangles[test_handle] = test_rectangle
-                    continue
-
-                if test_rectangle != previous_rectangles[test_handle]:
-                    widget.overlap_move_event(test_handle, test_rectangle)
-                    previous_rectangles[test_handle] = test_rectangle
-                    continue
-
-            for overlap_handle in previous_overlaps[widget_index] - current_overlaps:
-                last_location = previous_rectangles.pop(overlap_handle)
-                widget.overlap_exit_event(overlap_handle, last_location)
-
-            previous_overlaps[widget_index] = current_overlaps
-
-    def _scan_windows(self):
-        def window_selector(handle, argument):
-            rectangles, z_positions = argument
-            left, top, right, bottom = win32gui.GetWindowRect(handle)
-            rectangles.append(
-                (
-                    handle,
-                    QRect(left, top, right - left, bottom - top)
-                )
-            )
-            z_positions[handle] = len(z_positions)
-
-        rectangles = []
-        z_positions = {}
-        win32gui.EnumWindows(window_selector, (rectangles, z_positions))
-        self._rectangles = rectangles
-        self._z_positions = z_positions
-
-    def _timeout(self):
-        import time
-        print(time.time())
-        self._scan_windows()
-        self._check_for_overlap_changes()
 
 class Container(QFrame):
     mouse_over = Signal()
@@ -163,6 +91,8 @@ class Container(QFrame):
             SWP_ASYNCWINDOWPOS
         )
 
+        self._image = capture_window(self._handle)
+
     def paintEvent(self, event):
         if self._image is None:
             return
@@ -173,7 +103,7 @@ class Container(QFrame):
 
     def resizeEvent(self, event):
         self._sync_window()
-        self._image = capture_window(self._handle)
+        # self._image = capture_window(self._handle)
 
 class WindowSplitter(QFrame):
     mouse_over = Signal()
@@ -211,7 +141,7 @@ class WindowSplitter(QFrame):
         if self._root.moving:
             return
 
-        print('move', handle)
+        # print('move', handle)
 
     def overlap_exit_event(self, handle, rectangle):
         if self._root.moving:
@@ -221,16 +151,18 @@ class WindowSplitter(QFrame):
         if handle not in containers:
             return
 
+        self._root.remove_handle_to_move_under(handle)
         container = self._containers.pop(handle)
         container.deleteLater()
 
     def add_window(self, handle):
+        self._root.add_handle_to_move_under(handle)
         container = Container(self._root)
+        self._containers[handle] = container
         self._splitter.splitterMoved.connect(container.sync_needed)
         container.mouse_over.connect(self.mouse_over.emit)
-        container.grab_window(handle)
-        self._containers[handle] = container
         self._splitter.addWidget(container)
+        container.grab_window(handle)
 
 class Root(QFrame):
     moved = Signal()
@@ -245,23 +177,23 @@ class Root(QFrame):
 
         self._handles = set()
 
-        def window_selector(handle, argument):
-            class_name = win32gui.GetClassName(handle)
-            title = win32gui.GetWindowText(handle)
-            if class_name != "CabinetWClass":
-                return
-            # print(class_name, title)
-            argument.append(handle)
-        argument = []
-        win32gui.EnumWindows(window_selector, argument)
+        # def window_selector(handle, argument):
+        #     class_name = win32gui.GetClassName(handle)
+        #     title = win32gui.GetWindowText(handle)
+        #     if class_name != "CabinetWClass":
+        #         return
+        #     # print(class_name, title)
+        #     argument.append(handle)
+        # argument = []
+        # win32gui.EnumWindows(window_selector, argument)
 
-        overlap_monitor = WindowOverlapMonitor()
+        overlap_monitor = WindowMonitor()
 
         splitter = WindowSplitter(self, Qt.Vertical, overlap_monitor)
 
-        for handle in argument:
-            self._handles.add(handle)
-            splitter.add_window(handle)
+        # for handle in argument:
+        #     self._handles.add(handle)
+        #     splitter.add_window(handle)
 
         layout = QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
@@ -272,6 +204,12 @@ class Root(QFrame):
         self.exit_size_move.connect(self._move_under)
 
         self.moving = False
+
+    def add_handle_to_move_under(self, handle):
+        self._handles.add(handle)
+
+    def remove_handle_to_move_under(self, handle):
+        self._handles.remove(handle)
 
     def changeEvent(self, event):
         if event.type() == QEvent.ActivationChange:
@@ -302,6 +240,9 @@ class Root(QFrame):
 
     def _move_under(self):
         handles = self._handles
+
+        if not handles:
+            return
 
         # Get the z order
         def window_selector(handle, argument):
